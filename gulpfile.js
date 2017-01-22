da5d43a46c7423d044da92b84172e550e44ed7bb
Bhuvana Rajamani <codebee@MBP.local>
2017-01-21 21:21:12 -0600

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+const child = require('child_process');
+const browserSync = require('browser-sync').create();
+
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
+const css_minify = require('gulp-cssnano');
+const gutil = require('gulp-util');
+const scss = require('gulp-sass');
+
+const siteRoot = '_site';
+const cssFiles = '_sass/**/*.?(s)css';
+
+gulp.task('css', () => {
+  gulp.src(cssFiles)
+    .pipe(scss().on('error', scss.logError))
+    .pipe(concat('main.css'))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(css_minify())
+    .pipe(gulp.dest('assets'));
+});
+
+gulp.task('build', () => {
+  const jekyll = child.spawn('bundle', ['exec', 'jekyll', 'build', '--watch', '--incremental', '--drafts']);
+
+  const jekyllLogger = (buffer) => {
+    buffer.toString()
+      .split(/\n/)
+      .forEach((message) => gutil.log('Jekyll: ' + message));
+  };
+
+  jekyll.stdout.on('data', jekyllLogger);
+  jekyll.stderr.on('data', jekyllLogger);
+});
+
+gulp.task('serve', () => {
+  browserSync.init({
+    files: [siteRoot + '/**'],
+    notify: false,
+    open: false,
+    port: 4000,
+    server: {
+      baseDir: siteRoot
+    }
+  });
+
+  gulp.watch(cssFiles, ['css']);
+});
+
+gulp.task('default', ['css', 'build', 'serve']);
